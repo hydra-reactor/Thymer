d056a0cc72ecea5b198fa7308b36efd40de82344
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-03-30 13:59:51 +1100

    init

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var app = express();
 /*CONNECT TO DATABASE*/
 //make sure to uncomment which connection you're using
 
-var connection = 'mongodb://thyme:thyme@ds133340.mlab.com:33340/orion-thyme';
-// var connection = 'mongodb://localhost/thyme';
+// var connection = 'mongodb://thyme:thyme@ds133340.mlab.com:33340/orion-thyme';
+var connection = 'mongodb://localhost/thyme';
 mongoose.connect(connection);
 mongoose.connection.once('open', function() {
   console.log('Thyme after thyme on: ' + connection);
