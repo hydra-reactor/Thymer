a7e3dd1ea3310ff5c902b1182242ec1af4308a38
Darin Allen <darinallen@gmail.com>
2017-03-30 22:20:31 -0400

    viewRecipe Page working

--- client/partials/viewRecipe/viewRecipe.js
+++ client/partials/viewRecipe/viewRecipe.js
@@ -1,12 +1,7 @@
 angular.module('thymer.viewRecipe', [])
 
-.controller('viewRecipeController', function($scope, $location, $routeParams, Recipes) {
+.controller('viewRecipeController', function($scope, $location, $http, $routeParams, Recipes) {
   $scope.id = $routeParams.id;
-  $scope.title = 'Baked Ziti';
-  $scope.author = 'Chris Abrami';
-  $scope.time = 195;
-  $scope.cuisine = 'Italian';
-  $scope.servings = 4;
 
   $scope.timeCalculator = function(min) {
     var minRemain = min;
@@ -34,18 +29,6 @@ angular.module('thymer.viewRecipe', [])
     }
   };
 
-  $scope.timeDisplay = $scope.timeCalculator($scope.time);
-
-
-
-  $scope.image = "http://assets.kraftfoods.com/recipe_images/opendeploy/54590_640x428.jpg";
-  $scope.description = 'The best recipe ever.  Really, I think it is the best ever cause it is super tasty and very good.  Anyone in their right mind should go about trying to prepare it!  Just try to not love it!';
-  $scope.ingredients = ["2 cups flour", "1/2 tsp baking soda", "1/2 tsp salt", "3/4 cup unsalted butter, melted", "1 cup packed brown sugar", "1/2 cup white sugar", "1 tbs vanilla extract", "1 egg", "1 egg yolk", "2 cups chocolate chips"];
-  //comments do not yet exist in data.  Will likely be an array of objects.  Each object will likely have the following properties:  username, message, date
-  $scope.steps = [{description: "mix together brown sugar, garlic, liquid smoke, and crushed red peppers and saute over low heat.", totalMinutes: 5,type: "cookType"}, {description: "add young jack-fruit (drained) and simmer over medium heat", totalMinutes: 15, type: "cookType"}, {description: "add the BBQ and simmer", totalMinutes: 5, type: "cookType"}, {description: "serve as you would BBQ pork", totalMinutes: 0, type: "cookType"}];
-
-
-
   $scope.comments = [{username: 'abramicf', message: 'This is the best dish I have ever had.  Like, ever.  Totally recommended.', date: '12/10/2016'},{username: 'umaabrami', message: 'Not so sure about this one.  Felt a bit queasy afterwards, but maybe its just me', date: '10/10/2016'}, {username: 'abramicf', message: 'This is the best dish I have ever had.  Like, ever.  Totally recommended.', date: '12/10/2016'},{username: 'umaabrami', message: 'Not so sure about this one.  Felt a bit queasy afterwards, but maybe its just me', date: '10/10/2016'},{username: 'abramicf', message: 'This is the best dish I have ever had.  Like, ever.  Totally recommended.', date: '12/10/2016'},{username: 'umaabrami', message: 'Not so sure about this one.  Felt a bit queasy afterwards, but maybe its just me', date: '10/10/2016'},{username: 'abramicf', message: 'This is the best dish I have ever had.  Like, ever.  Totally recommended.', date: '12/10/2016'},{username: 'umaabrami', message: 'Not so sure about this one.  Felt a bit queasy afterwards, but maybe its just me', date: '10/10/2016'}];
 
   // get request to get a specific recipe by id
@@ -55,7 +38,8 @@ angular.module('thymer.viewRecipe', [])
       url: '/api/recipe/' + id
     }).then(function(res) {
       console.log('getRecipeById res.data: ', res.data);
-      $scope.recipeInfo = res.data;
+      $scope.recipe = res.data;
+      $scope.timeDisplay = $scope.timeCalculator(res.data.time);
       return res.data;
     });
   };
