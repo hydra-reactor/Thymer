516975e1a3187eca2adb4ec35cdde207d6215c1d
Chris Abrami <abramicf@gmail.com>
2017-03-31 16:20:53 -0700

    stops other audio from playing when new step is clicked and automatically starts new audio

--- client/partials/cooking/cooking.js
+++ client/partials/cooking/cooking.js
@@ -65,6 +65,7 @@ angular.module('thymer.cooking', [])
     }
   };
 
+
   //CREATE STEPSREMAINING AND STEPSPASSED ARRAYS BY SPLICING COOKSTEPTIMES AND COOKSTEPS - THESE ARE WHAT SHOULD BE ENTERED IN AND THEY'LL BE INITIALIZED WITH COOK STEPS AND CHANGED WITH THE FUNCTION
 
   // $scope.stepsRemaining = function(index) {
@@ -80,12 +81,14 @@ angular.module('thymer.cooking', [])
   //     });
   //   }
   // };
+  $scope.timeRemaining = $scope.recipe.time;
   $scope.stepsComplete = function(index) {
     //first, splice out everything from 0 through stepIndex
     //then, FOR EACH of those steps, apply the changes on those lines
     //STILL NEEDS TO ONLY DO CHECKBOX ONCE
-
+    $scope.stopCooking();
     var completeSteps = $scope.cookSteps.slice(0, index);
+    var remainingSteps = $scope.cookSteps.slice(index);
     for (var i = 0; i < completeSteps.length; i++) {
       //completed steps
     $('#step' + i).css({
@@ -94,6 +97,7 @@ angular.module('thymer.cooking', [])
         'font-weight': 'normal',
         'background-color': '#874B78'
       });
+    // $('#step' + i).removeChild('span');
         // adding in the check-mark glyphicon
     $('#step' + i + ' small').append('<span class="glyphicon glyphicon-ok-circle"></span>');
     $('.glyphicon').css({
@@ -102,11 +106,34 @@ angular.module('thymer.cooking', [])
       'color': '#E9EBE3'
     });
     }
+    console.log(remainingSteps);
+    var minRemain = 0;
+    for (var j = 0; j < remainingSteps.length; j++) {
+      minRemain += remainingSteps[j].totalMinutes;
+
+    }
+    $scope.timeRemaining = minRemain;
+    totalClock = FlipClock($('.total-cook'), $scope.timeRemaining * 60, {
+      clockFace: 'HourlyCounter',
+      countdown: true,
+      autoStart: false
+    });
+    totalClock.start();
+
+    console.log(remainingSteps[0].totalMinutes);
+    stepClock.setTime(remainingSteps[0].totalMinutes * 60);
+        // cb function
+          // $('#vid' + (i - 1)).removeAttr('autoplay');
+        stepClock.start(function() {
+          // appending audio onto each step
+          $('#vid' + (i - 1)).get(0).pause();
+          $('#vid' + i).append('<source src="http://api.voicerss.org/?key=c005359f68ec4ab89c485808abf9c53c&hl=en-gb&src=' + $scope.cookSteps[i].description + '"' + ' type="audio/mpeg">');
+        });
+
   };
 
-
-  // creates the total timer
-  var totalClock = FlipClock($('.total-cook'), $scope.recipe.time * 60, {
+    // creates the total timer
+  var totalClock = FlipClock($('.total-cook'), $scope.timeRemaining * 60, {
     clockFace: 'HourlyCounter',
     countdown: true,
     autoStart: false
@@ -114,7 +141,7 @@ angular.module('thymer.cooking', [])
 
   // var i set here to increment through cook-steps
   //THIS WILL CHANGE TO STEPINDEX
-  var i = 0;
+  var i = $scope.stepIndex;
 
   // sets the clock format based on step-duration
   var clockFormat = 'MinuteCounter';
@@ -160,39 +187,45 @@ angular.module('thymer.cooking', [])
 
 
 //checks to see whether the step timer is done
-  setInterval(function() {
-    // if step timer is done, increment to the next step
-    if (!stepClock.face.factory.running && cookingStarted) {
-      i++;
-      var nextStepTime = $scope.cookStepTimes[i] * 60;
-      //stops the step timer once the incrementor is the array length
-      if (i >= $scope.cookStepTimes.length) {
-        stepClock.stop();
-      }
-      else {
-        // styles specific steps based on whether they are active or complete
-        $('#step' + i).css(activeStyle);
-        $('#step' + (i-1)).css({
-          'color': '#E9EBE3',
-          'font-size': '14px',
-          'font-weight': 'normal',
-          'background-color': '#874B78'
-        })
-        // adding in the check-mark glyphicon
-        $('#step' + (i-1) + ' small').append('<span class="glyphicon glyphicon-ok-circle"></span>');
-        $('.glyphicon').css({
-          'font-size': '25px',
-          'float': 'right',
-          'color': '#E9EBE3'
-        });
-        // initiating the next step countdown
-        stepClock.setTime(nextStepTime);
-        // cb function
-        stepClock.start(function() {
-          // appending audio onto each step
-          $('#vid' + i).append('<source src="http://api.voicerss.org/?key=c005359f68ec4ab89c485808abf9c53c&hl=en-gb&src=' + $scope.cookSteps[i].description + '"' + ' type="audio/mpeg">')
-        });
-      }
-    };
-  }, 1000);
+//THIS HAS TO BE WRAPPED INSIDE A FUNCTION THAT CAN BE INVOKED
+
+  // $scope.goThroughSteps = function() {
+    setInterval(function() {
+      // if step timer is done, increment to the next step
+      if (!stepClock.face.factory.running && cookingStarted) {
+        i++;
+        var nextStepTime = $scope.cookStepTimes[i] * 60;
+        //stops the step timer once the incrementor is the array length
+        if (i >= $scope.cookStepTimes.length) {
+          stepClock.stop();
+        }
+        else {
+          // styles specific steps based on whether they are active or complete
+          $('#step' + i).css(activeStyle);
+          $('#step' + (i-1)).css({
+            'color': '#E9EBE3',
+            'font-size': '14px',
+            'font-weight': 'normal',
+            'background-color': '#874B78'
+          })
+          // adding in the check-mark glyphicon
+          $('#step' + (i-1) + ' small').append('<span class="glyphicon glyphicon-ok-circle"></span>');
+          $('.glyphicon').css({
+            'font-size': '25px',
+            'float': 'right',
+            'color': '#E9EBE3'
+          });
+          // initiating the next step countdown
+          stepClock.setTime(nextStepTime);
+          // cb function
+          stepClock.start(function() {
+            // appending audio onto each step
+            $('#vid' + i).append('<source src="http://api.voicerss.org/?key=c005359f68ec4ab89c485808abf9c53c&hl=en-gb&src=' + $scope.cookSteps[i].description + '"' + ' type="audio/mpeg">')
+          });
+        }
+      };
+    }, 1000);
+  // };
+  // $scope.goThroughSteps();
+
 });
